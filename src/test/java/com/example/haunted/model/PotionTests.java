527a78dc5e18f7e36678e1e76c269478a7e01fa1
Brett Pearson <131393660+BPear1225@users.noreply.github.com>
2026-04-14 21:50:31 -0500

    Started Potion tests

---
+++ src/test/java/com/example/haunted/model/PotionTests.java
@@ -0,0 +1,56 @@
+package com.example.haunted.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PotionTests {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	private static Stream<Arguments> creationFailCases() {
+		return Stream.of(Arguments.of(null, null, 0), Arguments.of(null, "Description", 0),
+				Arguments.of("Name", null, 0));
+	}
+
+	@ParameterizedTest
+	@MethodSource("creationFailCases")
+	public void testImproperCreation(String name, String description, int defense) {
+		assertThrows(NullPointerException.class, () -> new Potion(name, description, defense));
+	}
+
+	private static Stream<Arguments> successfulCreationCases() {
+		return Stream.of(Arguments.of(new Potion("Water", "Some refreshing water", 0), 0),
+				Arguments.of(new Potion("Health Potion", "Heals 10 Health", 10), 10),
+				Arguments.of(new Potion("Damage Potion", "Heals -10 Health", -10), -10));
+	}
+
+	@ParameterizedTest
+	@MethodSource("successfulCreationCases")
+	void testGetHealingAmount(Potion item, int expectedOutput) {
+		assertEquals(item.getHealingAmount(), expectedOutput);
+	}
+
+}
